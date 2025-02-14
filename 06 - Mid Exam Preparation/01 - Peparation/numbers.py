916193e53c126f118a1c5d260d7305b336db661a
Preslav Kolev <kolevpreslav91@gmail.com>
2025-02-14 19:35:29 +0200

    Solved some problems.

--- 06 - Mid Exam Preparation/01 - Peparation/numbers.py
+++ 06 - Mid Exam Preparation/01 - Peparation/numbers.py
@@ -1,4 +1,4 @@
-def sorting(numbers: list) -> str:
+def sorting(numbers: list):
     sorted_numbers = []
 
     avg = sum(numbers) / len(numbers)
@@ -7,7 +7,7 @@ def sorting(numbers: list) -> str:
             if i < 5:
                 sorted_numbers.append(str(num))
 
-    print(" ".join(sorted_numbers))
+    return " ".join(sorted_numbers)
 
 
 
@@ -17,7 +17,7 @@ numbers = sorted(map(int,input().split()), reverse=True)
 if numbers[0] == 1 and len(numbers) == 1:
     print("No")
 else:
-    sorting(numbers)
+    print(sorting(numbers))
 
 
 
