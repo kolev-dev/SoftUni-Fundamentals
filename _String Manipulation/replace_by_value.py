be17ad4b4547244de6b7cf01b6d4ec54faf2854c
Preslav Kolev <Kolevpreslav91@Gmail.com>
2025-02-25 21:03:01 +0200

    Solved some problems.

--- _String Manipulation/replace_by_value.py
+++ _String Manipulation/replace_by_value.py
@@ -6,3 +6,9 @@ print(question.replace("Hello", "Hi"))
 # in order this to work, we have to store the value of the replacement
 
 print(question)
+
+txt = "Hello, welcome to my world."
+
+x = txt.startswith("Hello")
+
+print(x)
