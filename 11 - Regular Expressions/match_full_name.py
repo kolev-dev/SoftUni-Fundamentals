6c5eb70b97004990306e3b91e9dd7faf8cf6ceed
Preslav Kolev <Kolevpreslav91@Gmail.com>
2025-03-17 16:13:59 +0200

    Solved some problems.

--- 11 - Regular Expressions/match_full_name.py
+++ 11 - Regular Expressions/match_full_name.py
@@ -1,5 +1,7 @@
 import re
 
-name = input()
-set_of_names = set(re.findall(r"\b[A-Z]{1}[a-z]*\b", name))
+names = input()
+set_of_names = re.findall(r"\b[A-Z][a-z]* [A-Z][a-z]*\b", names)
 print(" ".join(set_of_names))
+
+# \b[A-Z]{1}[a-z] [A-Z]{1}[a-z]*\b
--- 11 - Regular Expressions/match_phone_number.py
+++ 11 - Regular Expressions/match_phone_number.py
@@ -2,4 +2,5 @@ import re
 
 phone_numbers = input()
 
-match = re.findall(r'\+359[" "|"-"]')
+match = re.findall(r'\+359 \d \d{3} \d{4}|\+359-\d-\d{3}-\d{4}$', phone_numbers)
+print(", ".join(match))
